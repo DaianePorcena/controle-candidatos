c28d1ed0f71d88064b2df8a5fd9cfd2950564e43
DaianePorcena <daiporcenna@gmail.com>
2024-05-20 14:19:05 -0300

    ✨ feat: add function to contact the candidate

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,43 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
     System.out.println("Processo Seletivo - Candidatura de Emprego");
-    imprimirSelecionados();
+    String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO" };
+    for (String candidato : candidatos) {
+      entrandoEmContato(candidato);
+    }
+  }
+
+  static void entrandoEmContato(String candidato) {
+    int tentativasRealizadas = 1;
+    boolean continuarTentando = true;
+    boolean atendeu = false;
+    do {
+      atendeu = atender();
+      continuarTentando = !atendeu;
+      if (continuarTentando) {
+        tentativasRealizadas++;
+      } else {
+        System.out.println("CONTATO REALIZADO COM SUCESSO");
+      }
+    } while (continuarTentando && tentativasRealizadas < 3);
+
+    if (atendeu) {
+      System.out.println("CONSEGUIMOS CONTATO COM O CANDIDATO " + candidato + " APÓS " + tentativasRealizadas
+          + " TENTATIVAS");
+    } else {
+      System.out.println("NÃO CONSEGUIMOS CONTATO COM O CANDIDATO " + candidato + ", NÚMERO MÁXIMO DE "
+          + tentativasRealizadas + " TENTATIVAS REALIZADAS");
+    }
+  }
+
+  // método auxiliar
+  static boolean atender() {
+    return new Random().nextInt(3) == 1;
   }
 
   static void imprimirSelecionados() {
